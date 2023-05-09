2f271cec9d5c0b8778a9b59d7c122afed1db361c
VioletYSL <harrylee619344@gmail.com>
2023-05-09 17:21:39 +0800

    0509

--- MOMOcrawler.js
+++ MOMOcrawler.js
@@ -1,33 +1,37 @@
+//引入puppeteer模組
 const puppeteer = require('puppeteer');
+//引入MySQL模組
 const mysql = require('mysql');
-
+//設定連線資訊
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'root',
     database: 'item',
 });
-
-const name = "nike air force"; // 、nike airmax97、jordan、nike dunk、nike air force
+//定義搜尋關鍵字
+const name = "nike air force"; 
+//將關鍵字的空格做處理
 const newName = name.replace(" ", "%20");
-// i 為第幾頁 momo從1開始
-// const i = 1;
 
+//定義一個delay函數，用於設置延遲時間。
 function delay(time) {
     return new Promise(function (resolve) {
         setTimeout(resolve, time)
     });
 };
-
+//操作puppeteer
 async function main(newName, i) {
     const browser = await puppeteer.launch({ headless: false });
+    //開啟瀏覽器
     const page = await browser.newPage();
-    console.log('開啟瀏覽器');
+    //設定網頁長寬
     await page.setViewport({ width: 1920, height: 1080 });
+    //宣告要前往的URL
     const shopurl = "https://www.momoshop.com.tw/search/searchShop.jsp?keyword=" + newName + "&searchType=1&curPage=" + i + "&_isFuzzy=0&showType=chessboardType";
-
-    console.log('前往指定的網頁')
+    //讓瀏覽器前往設定的網址
     await page.goto(shopurl, { waitUntil: 'networkidle2' });
+    //設定延遲
     await delay(1000);
     // ...scraping code here...
     const elem = await page.$('div');
@@ -48,10 +52,7 @@ async function main(newName, i) {
 
     var dataList = await page.evaluate(() => {
         let result = [];
-        // const $ = window.$;
-        // let liList = document.querySelector("#BodyBase > div.bt_2_layout.searchbox.searchListArea.selectedtop > div.searchPrdListArea.bookList > div.listArea > ul > li");
-
-        // document.querySelector("a > div.prdImgWrap.prdListSwiper.swiper-container.manyPics.swiper-container-initialized.swiper-container-horizontal > div.swiper-wrapper > div.swiper-slide.swiper-slide-active > img")
+       
 
         for (let index = 1; index <= 30; index++) {
 
@@ -82,17 +83,18 @@ async function main(newName, i) {
 
     return dataList; // resolve the Promise with the scraped data
 }
-
+// i 為第幾頁 momo從1開始
+// const i = 1;
 (async () => {
-    for(let i = 2;i<3;i++){
+    for(let i = 1;i<3;i++){
 
         const dataList = await main(newName, i);
         dataList.forEach((element) => {
             connection.query(`INSERT INTO sneakers (url,img,name,price,source,sourceimg,subtitle,type) VALUES("${element.url}","${element.img}","${element.name}","${element.price}","${element.source}","${element.sourceimg}","${element.subtitle}","${element.type}")`, function (error, results, fields) {
 
                 if (error) throw error;
-                console.log('資料庫新增成功');
-                // console.log(results);
+                // console.log('資料庫新增成功');
+                console.log(results);
             }); 
         });
     }
